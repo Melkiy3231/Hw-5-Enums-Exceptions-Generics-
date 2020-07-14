4caa2387b123be6cc91ecaa5cccc131c2a10e4a1
Yaroslav <67073206+Melkiy3231@users.noreply.github.com>
2020-07-14 21:19:47 +0300

    Update Main.java

--- src/main/java/com/yaroslavshchur/Main.java
+++ src/main/java/com/yaroslavshchur/Main.java
@@ -1,19 +1,12 @@
 package main.java.com.yaroslavshchur;
-
-
 import main.java.com.yaroslavshchur.generic.MyList;
-
-
 public class Main {
-
     public static void main(String[] args) {
-
         MyList<Integer> myList = new MyList<>();
-        myList.add(3);
-        myList.add(32);
-        myList.add(26);
-
-        System.out.println("Largest number: " + myList.getMaxValue());
-        System.out.println("Smallest number: " + myList.getMinValue());
+        myList.add(112);
+        myList.add(1333);
+        myList.add(92);
     }
+         System.out.println("List - 1 Min = "+myList.getMin());
+         System.out.println("List - 1 Max = "+myList.getMax());
 }
